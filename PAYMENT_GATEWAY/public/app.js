dd8ff28b103131695444bfc181fe66ecc5be4a9d
Tanish Mohanty <tanish0911@gmail.com>
2021-09-08 21:53:56 +0530

    Payment
    designed payment gateway with backend

---
+++ PAYMENT_GATEWAY/public/app.js
@@ -0,0 +1,19 @@
+
+// firebase setup
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDn0YLSUKggpOhhhnI5UBREb0QuEo1nyJs",
+    authDomain: "fluffy-2ea00.firebaseapp.com",
+    databaseURL: "https://fluffy-2ea00.firebaseio.com",
+    projectId: "fluffy-2ea00",
+    storageBucket: "fluffy-2ea00.appspot.com",
+    messagingSenderId: "72613846505",
+    appId: "1:72613846505:web:00e7d761ab4af2b4f13f78",
+    measurementId: "G-G141S8YW41"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
