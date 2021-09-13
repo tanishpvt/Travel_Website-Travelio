1352976f361e59540c15e494e041135e1ec02c07
Tanish Mohanty <tanish0911@gmail.com>
2021-09-13 13:31:13 +0530

    Youtube
    Added Youtube section in home

--- javascript.js
+++ javascript.js
@@ -74,3 +74,35 @@
       });
   });
   
+  // YOUTUBE
+
+  $(function() {
+    var $allVideos = $("iframe[src^='https://www.youtube.com/embed/35npVaFGHMY'], iframe[src^='https://www.youtube.com/embed/35npVaFGHMY'], object, embed"),
+        $fluidEl = $("figure");
+
+    $allVideos.each(function() {
+        $(this)
+            // jQuery .data does not work on object/embed elements
+            .attr('data-aspectRatio', this.height / this.width)
+            .removeAttr('height')
+            .removeAttr('width');
+    });
+    $(window).resize(function() {
+        var newWidth = $fluidEl.width();
+        $allVideos.each(function() {
+            var $el = $(this);
+            $el
+                .width(newWidth)
+                .height(newWidth * $el.attr('data-aspectRatio'));
+        });
+    }).resize();
+});
+
+//LOADING SCREEN
+jQuery(window).load(function() {
+    //$(".loader-centered").fadeOut();
+    //in production change 5000 to 400
+    $(".loader").delay(3000).fadeOut("slow");
+    $("#loading-text").addClass('text-success').html('page loaded');
+    
+});
